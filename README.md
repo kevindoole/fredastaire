f518044d1e2abed83ddf4dc61a9a3676726cf2b3
Kevin Doole <kdoole@gmail.com>
2017-06-26 10:36:51 -0500

    Fixes various mistakes and makes the readme a bit more helpful

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # mocha-given
-Mocha-given adds a `given` method, which makes it really easy to succinctly and readably set up a test case.
+Mocha-given adds a `given` method to mocha tests. It has two impacts:
+- Your test cases' setup code is significantly more readable
+- You only have to write the same setup steps once
+
+_Example_
+`given(name, {within, withArgs, ...whateverYouWantToPassToTheDefinition})`
+
+## Setup
+0. `npm install mocha-given --save-dev`
+1. Make your step definitions file wherever you like.
+2. Write some tests that use `given`
+3. Run your tests with the appropriate options:
+  `mocha ./tests/some-tests.js --ui mocha-given --setupDefinitions ./tests/wherever/step-definitions.js`
+
+## Usage
+You need two things: mocha tests that call `given` and some step definitions.
+
+### 1. Tests
+Just call `given('with some string')`. The string will be used to identify which step definition to call (more on that later).
+
+There's also `when` and `and`, which are aliases of `given`.
+
+Calls to `given` return the mocha-given module, so you can chain calls together into wonderful poems.
+
+_/your/project/tests/astronauts-test.js_
+```JavaScript
+const state = {
+	spaceThings: [],
+};
+
+let res;
+
+describe('Installing interesting space apparatus', function() {
+	given('there are two spacethings', {within: state.spaceThings});
+	// Calls steps['there are two spacethings']().
+
+	when('i request a space thing', {
+		id: state.spaceThings[0].id,
+		within: res,
+	});
+	// "when" is an alias of given. So is "and".
+
+	it('should give me one space thing', function() {
+	  expect(res.body.spacething).to.not.be.null;
+	  expect(res.body.spacething.id).to.equal(this.spacethings[0].id);
+	});
+});
+```
+
+### 2. Step definitions
+Step definitions are the places where you put your setup code for individual tests.
+
+_/your/project/wherever/you/like/your-definitions.js_
+```
+module.exports = {
+  'there are two space things': function() {
+    SpaceThings.create({idunno: 'stuff'});
+    SpaceThings.create({idunno: 'more stuff'});
+  },
+
+  'there is one astronaut': function() {
+    return SpaceAstronaut.create();
+  },
+
+  'one space thing is requested': function({id, container}) {
+    return request.get('/space-things/' + id)
+    .then(response => container = response);
+  }
+};
+```
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+var fs = require('fs');
+var path = require('path');
 var Mocha = require('mocha');
 var argv = require( 'minimist' )( process.argv );
-var fs = require('fs');
 var colors = require( 'colors/safe' );
 
 /**
@@ -12,29 +13,70 @@ module.exports = Mocha.interfaces['mocha-given'] = function(suite) {
 
     context.run = mocha.options.delay && common.runWithSuite(suite);
 
+    var setupDefinitionsArgumentProvided = !!argv.setupDefinitions;
+
+    if (!setupDefinitionsArgumentProvided) {
+      warn('You must provide your step definitions file location using the --setupDefinitions option.')
+
+      throw new Error('mocha-given: Called `given` without a step definitions file');
+    }
+
+    var fullPathToStepDefinitions = path.join(process.env.PWD, argv.setupDefinitions)
+    var stepDefinitionsFileExists = fs.existsSync(argv.setupDefinitions);
+    var stepDefinitions = require(fullPathToStepDefinitions);
+
     /**
      * Enables easily sharing setup steps.
      */
-    context.given = function(title) {
-      var setupDefinitionsAvailable = argv.setupDefinitions && fs.exists(argv.setupDefinitions);
-      if (!setupDefinitionsAvailable) {
-        warn('Skipping setup -- your setup definitions file (' + argv.setupDefinitions + ') does not exist.');
+    context.given = function(title, givenOptions) {
+      if (!givenOptions) {
+        givenOptions = {};
+      }
+
+      if (!stepDefinitionsFileExists) {
+        warn('mocha-given: Aborting');
+        info('The step definitions filename you provided (' + argv.setupDefinitions + ') does not exist.')
 
         return;
       }
 
-      var setupDefinitions = require(argv.setupDefinitions);
-      if (!setupDefinitions[title]) {
-        warn('Skipping unimplemented setup definition: ' + argv.setupDefinitions);
+      if (!stepDefinitions[title]) {
+        warn('mocha-given: Skipping unimplemented step definition.');
+        info('Add the method "' + title + '" to ' + argv.setupDefinitions + '.');
 
         return;
       }
 
-      setupDefinitions[title]();
+      var stepOptions = Object.assign({}, givenOptions);
+
+      // Map within to container, because it reads better on the definition side.
+      if (givenOptions.within) {
+        stepOptions.container = givenOptions.within;
+      }
+
+      // Map withArgs to args, because it reads better on the definition side.
+      if (givenOptions.withArgs) {
+        if (!Array.isArray(givenOptions.withArgs)) {
+          givenOptions.withArgs = [givenOptions.withArgs];
+        }
+        stepOptions.args = givenOptions.withArgs;
+      }
+
+      stepDefinitions[title](stepOptions);
+
+      return context;
     };
+
+
+    context.when = context.given;
+    context.and = context.given;
   });
 };
 
 function warn(msg) {
-  console.warn( colors.yellow( '\n' + msg + '\n' ) );
+  console.warn( colors.yellow.bold( '\n' + msg ) );
+}
+
+function info(msg) {
+  console.info( colors.gray.italic( msg ) );
 }
